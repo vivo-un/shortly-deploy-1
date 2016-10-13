b1676d2ac10cb116231460349090131c2afac916
Nam Huynh <nthwinn@gmail.com>
2016-10-13 11:23:24 -0700

    Add concat before deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        seperator: ';'
+      },
+      dist: {
+        src: ['app/config.js','models/link.js', 'models/user.js', 'collections/links.js', 'collections/users.js']
+        dest: ['build.js']
+      }
     },
 
     mochaTest: {
@@ -76,7 +83,7 @@ module.exports = function(grunt) {
     'mochaTest'
   ]);
 
-  grunt.registerTask('build', [
+  grunt.registerTask('build', ['concat'
   ]);
 
   grunt.registerTask('upload', function(n) {
